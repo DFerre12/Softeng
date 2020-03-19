b7677415ac44728107c78f4972c8f44b3bfd2d09
DFerre12 <55858672+DFerre12@users.noreply.github.com>
2020-03-19 12:37:33 +0000

    Cured AIDS
    Removed duplicate files and tidied files to avoid conflicts and confusion.

--- JS/server.js
+++ JS/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-
+const port = 8080
 const { client } = require('pg')
 
-
+//Sends response to the client
+app.get('/', (req, res) => res.status(200).send('Yeah it worked...'))
+//Listens for a request on port 8080
+app.listen(port, () => console.log (`listening on port ${port}`))
 
 app.set('view engine', 'ejs')
